bf7624d8335a0bfdcf694e195f5a25a73f564bd0
Mitsuyuki Shiiba <bufferings@gmail.com>
2024-01-22 23:41:29 +0900

    For 20240122

--- infra/main.tf
+++ infra/main.tf
@@ -162,6 +162,33 @@ resource "aws_service_discovery_http_namespace" "namespace" {
 }
 
 #########################################
+# ECS Role
+#########################################
+
+resource "aws_iam_role" "task_execution" {
+  name = "${local.main_name}-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = ""
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution" {
+  role       = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+#########################################
 # Output
 #########################################
 
